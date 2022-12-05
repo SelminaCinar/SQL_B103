16e6125d835619a2ca6cbafe174910aff0c80369
SelminaCinar <selminacinar@gmail.com>
2022-12-05 23:49:32 +0300

    sql

--- day01.sql
+++ day01.sql
@@ -1,28 +1,27 @@
-CREATE TABLE ogrenciler1
+-- DATABASE(VeriTabanı) Oluşturma
+Create dataBase evren; 
+create database erol;
+-- DDL - DATA DEFINITION LANG.
+-- CREATE - TABLO OLUŞTURMA
+CREATE TABLE ogrenciler2
 (
 ogrenci_no char(7),
 isim varchar(20),
 soyisim varchar(25),
 not_ort real,-- Ondalıklı sayılar için kullanılır(Double gibi)
-kayit_tarih date
+kayit_tarih date    
 );
-
---Varolan tablodan yeni tablo olusturma
+--VAROLAN TABLODAN YENİ BİR TABLO OLUŞTURMA
 CREATE TABLE ogrenci_notlari
-as--Benzer tablodaki başlıklarla ve data tipleriyle yeni bir tablo oluşturmak için
+AS -- Benzer tablodaki başlıklarla ve data tipleriyle yeni bir tablo oluşturmak için 
 --normal tablo oluştururken ki parantezler yerine AS kullanıp Select komutuyla almak istediğimiz verileri alırız
-select isim,soyisim,not_ort from ogrenciler;
-
---DATA MANIPULATION LANG. DML
---INSERT (DATABASE VERI EKLEME)
-
-iNSERT iNTO ogrenciler VALUES ('1234567','Said','Ilhan',85.5,now());
-iNSERT iNTO ogrenciler VALUES ('1234567','Toprak Gezi','Cinar',85.5,'2022-12-01');
-
---BIR TABLOYA PARCALI VERI EKLEMEK ICIN
-INSERT INTO ogrenciler(isim,soyisim) VALUES ('SELMINA','CINAR');
-
---DQL - DATA QUERY LANG.
---SELECT
-
-select * from ogrenciler;
+SELECT isim,soyisim,not_ort FROM ogrenciler2;
+-- DML - DATA MANUPULATION LANG.
+-- INSERT (Database'e veri ekleme)
+INSERT INTO ogrenciler2 VALUES ('1234567','Said','ILHAN',85.5,now());
+INSERT INTO ogrenciler2 VALUES ('1234567','Said','ILHAN',85.5,'2020-12-11');
+-- BIR TABLOYA PARÇALI VERİ EKLEMEK İSTERSEK
+INSERT INTO ogrenciler2 (isim,soyisim) VALUES ('Erol','Evren');
+-- DQL - DATA QUERY LANG.
+-- SELECT
+select * FROM ogrenciler2; -- Burdaki * sembolü herşeyi anlamındadır
